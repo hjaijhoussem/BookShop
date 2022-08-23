8c4a1eab2a0a86f5962e9a761737a6e3b4bb7e7d
hjaijhoussem <hjaijhoussem@gmail.com>
2022-08-23 23:27:16 +0100

    refactoring function

--- models/product.js
+++ models/product.js
@@ -2,35 +2,36 @@ const { builtinModules } = require('module');
 const path = require('path');
 const fs = require('fs');
 
+const p =  path.join(path.dirname(process.mainModule.filename),'data','products.json');
+
+const getProductsFromFile = (cb) => {
+    fs.readFile(p,(err,fileContent)=>{
+        if(err){
+            cb([]);
+        }else{
+            cb(JSON.parse(fileContent));
+        }
+    });
+}
+
 module.exports = class Product {
     constructor(title){
         this.title = title;
     }
 
     save(){
-        const p = path.join(path.dirname(process.mainModule.filename),'data','products.json');
-        fs.readFile(p,(err,fileContent)=>{
-            // fill the products array with the data in the file
-            let products = [];
-            if(!err){
-                products = JSON.parse(fileContent); 
-            };
+        
+        getProductsFromFile(products =>){
             //adding the new product to products array
             products.push(this);
             // replace the content of the file with the data in products array that already contains the old + new added data
             fs.writeFile(p,JSON.stringify(products),(err)=>{
                 console.log(err);
             });
-        });
-    }
+        };
+    };
     //JSON.stringify(arg) : turn the data to string
     static fetchAll(cb){ // cb a function 
-        const p = path.join(path.dirname(process.mainModule.filename),'data','products.json');
-        fs.readFile(p,(err,fileContent)=>{
-            if(err){
-                cb([]);
-            }
-            cb(JSON.parse(fileContent));
-        });
+        getProductsFromFile(cb);
     }
 }
