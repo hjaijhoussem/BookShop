171fa8ae34243c3333b09ef914a90727ceda0e09
hjaijhoussem <hjaijhoussem@gmail.com>
2022-08-29 22:08:01 +0100

    copiying all project

--- models/cart.js
+++ models/cart.js
@@ -1,4 +1,3 @@
-const { builtinModules } = require('module');
 const fs = require('fs');
 const path = require('path');
 
--- models/product.js
+++ models/product.js
@@ -1,47 +1,48 @@
-const { builtinModules } = require('module');
-const path = require('path');
 const fs = require('fs');
+const path = require('path');
 
-const p =  path.join(path.dirname(process.mainModule.filename),'data','products.json');
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
 
-const getProductsFromFile = (cb) => {
-    fs.readFile(p,(err,fileContent)=>{
-        if(err){
-            cb([]);
-        }else{
-            cb(JSON.parse(fileContent));
-        }
-    });
-}
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
-    constructor(title,imageUrl,description,price){
-        this.title = title;
-        this.imageUrl = imageUrl;
-        this.description = description;
-        this.price = price; 
-    }
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
 
-    save(){
-        this.id = Math.random().toString();
-        getProductsFromFile(products =>{
-            //adding the new product to products array
-            products.push(this);
-            // replace the content of the file with the data in products array that already contains the old + new added data
-            fs.writeFile(p,JSON.stringify(products),(err)=>{
-                console.log(err);
-            });
-        });
-    };
-    //JSON.stringify(arg) : turn the data to string
-    static fetchAll(cb){ // cb a function 
-        getProductsFromFile(cb);
-    }
+  save() {
+    this.id = Math.random().toString();
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
 
-    static findById(id, cb) {
-        getProductsFromFile(products => {
-            const product = products.find(p => p.id ===id);
-            cb(product);
-        });
-    }
-}
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+
+  static findById(id, cb) {
+    getProductsFromFile(products => {
+      const product = products.find(p => p.id === id);
+      cb(product);
+    });
+  }
+};
