a597f27b135a30beeaf255419c658ea1ccc6aedd
hjaijhoussem <hjaijhoussem@gmail.com>
2022-09-14 21:35:01 +0100

    adding user model

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+const User = require('./models/user');
+
 
 const errorController = require('./controllers/error');
 
@@ -18,8 +20,17 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
 app.use((req, res, next) => {
-    next();
+    User.findById('632236ab2d788e3194e206e8')
+        .then( user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => {
+            console.log(err);
+        });
 });
 
 app.use('/admin', adminRoutes);
@@ -29,6 +40,18 @@ app.use(errorController.get404);
 
 mongoose.connect('mongodb+srv://hjaiejhoussem:20819535h@cluster0.atlc1l6.mongodb.net/shop?retryWrites=true&w=majority')
         .then(result => {
+            User.findOne().then(user => {
+                if(!user) {
+                    const user = new User({
+                        name: 'jabeur',
+                        email: 'jabeur@test.com',
+                        cart: {
+                            items: []
+                        }
+                    });
+                    user.save();
+                }
+            });
             app.listen(3000);	    
         })
         .catch(err => {
