ea245a74ab7deebed3ef13343ac9481bc28db8ba
hjaijhoussem <hjaijhoussem@gmail.com>
2022-09-14 20:55:45 +0100

    deleting product

--- controllers/admin.js
+++ controllers/admin.js
@@ -87,8 +87,14 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
-/* exports.postDeleteProduct = (req, res, next) => {
+exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect('/admin/products');
-}; */
+  Product.findByIdAndRemove(prodId)
+         .then(() => {
+          console.log('PRODUCT DESTROYED');
+          res.redirect('/admin/products');
+         })
+         .catch(err => {
+          console.log(err);
+         })
+}; 
